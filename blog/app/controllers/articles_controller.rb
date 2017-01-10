6692e0d80fcdcd12a9be02a5fc2fdda437d6a842
tdhuynh <tommyhuynh93@gmail.com>
2017-01-10 15:22:09 -0500

    starting index html of article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -9,7 +13,6 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
     @article.save
     redirect_to @article
   end
@@ -18,4 +21,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :text)
     end
+
 end
